c6e5c07663c62a8bfcdadf26abe504dae25b07c1
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-09-25 20:22:42 +0900

    add

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,23 @@
 import React from "react"
 import { render } from "react-dom"
+import useForm from "react-hook-form"
+import { Input, Button } from "@storybook/design-system"
 
 const App = () => {
-  return <div>hello</div>
+  const { register, handleSubmit } = useForm()
+  return (
+    <div>
+      hello
+      <form
+        onSubmit={handleSubmit((e) => {
+          console.log(e)
+        })}
+      >
+        <Input name="title" ref={register({ required: true })}></Input>
+        {/* <Button>send</Button> */}
+      </form>
+    </div>
+  )
 }
 
 render(<App />, document.querySelector("#root"))
